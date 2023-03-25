f7af39747fa69b34e223c0f59d7e498b614912fd
Abu Desai <abhyuday.desai@gmail.com>
2023-03-25 15:46:50 -0700

    update to comments inside paths

--- app/paths.py
+++ app/paths.py
@@ -4,7 +4,7 @@ import os
 # Path to the current file's directory
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Path to inputs inside ml_vol
+# Path to inputs 
 INPUT_DIR = os.path.join(CURRENT_DIR, "inputs")
 # File path for input schema file 
 SCHEMA_DIR = os.path.join(INPUT_DIR, "data_config")
@@ -15,12 +15,12 @@ TRAIN_DIR = os.path.join(DATA_DIR, "training")
 # Path to test directory inside data directory
 TEST_DIR = os.path.join(DATA_DIR, "testing")
 
-# Path to model directory inside ml_vol
+# Path to model directory 
 MODEL_PATH = os.path.join(CURRENT_DIR, "model")
 # Path to artifacts directory inside model directory
 MODEL_ARTIFACTS_PATH = os.path.join(MODEL_PATH, "artifacts")
 
-# Path to outputs inside ml_vol
+# Path to outputs 
 OUTPUT_DIR = os.path.join(CURRENT_DIR, "outputs")
 # Path to errors directory inside model directory
 LOGS_DIR = os.path.join(OUTPUT_DIR, "logs")
